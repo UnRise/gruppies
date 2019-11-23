9fd48b3336b74e43bdb53073dbc713b4ffa805ca
Ubuntu <ubuntu@ip-172-31-22-66.us-east-2.compute.internal>
2019-11-23 09:37:30 +0000

    govno

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const path = require('path');
 const app = express()
 //server config
 const port = 8080
-const host = '18.218.7.170'
+const host = '172.31.22.66'
 //attack config
 
+app.get('/', function(req, res){
+   res.send("Hello World!");
+});
 
 app.get('/start', function(req, res){
     console.log('Someone visit (/start) page')
@@ -20,6 +23,6 @@ app.get('/attack/:number', (req, res) => {
     res.send(`successful attack on ${victimsNumber}`)
 })
 
-app.listen(port, function(reg, res){
+app.listen(port, host, function(reg, res){
     console.log(`Sever listen on http://${host}:${port}`)
 })
